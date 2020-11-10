e1eb155e3c1494a27cfca4194eef182f2eeb8397
logic-fabric <lowic@logic-fabric.fr>
2020-11-10 11:40:46 +0100

    feature: add fizzbuzz.js

---
+++ classic-puzzles/fizzbuzz.js
@@ -0,0 +1,30 @@
+/*
+Wording:
+-------
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of 3 it should output “Fizz” instead of the number and for the multiples of 5 output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
+
+function fizzbuzz(n) {
+    let output = [];
+
+    if (n < 1) {
+        return output;
+    } else {
+        for (let i = 1; i <= n; i++) {
+            if (i % 15 === 0) {
+                output.push("FizzBuzz");
+            } else if (i % 3 === 0) {
+                output.push("Fizz");
+            } else if (i % 5 === 0) {
+                output.push("Buzz");
+            } else {
+                output.push(String(i));
+            }
+        }
+        return output;
+    }
+}
+
+console.log(fizzbuzz(15));
