e78ffac9ef113eba024ec3eaaac69001a75d8069
logic-fabric <lowic@logic-fabric.fr>
2020-11-16 07:58:19 +0100

    refactor: add typing to common_elements.py

--- classic-puzzles/puzzles-with-arrays/common_elements.py
+++ classic-puzzles/puzzles-with-arrays/common_elements.py
@@ -5,7 +5,7 @@ Find the common elements (as a sorted array) between two sorted arrays of intege
 """
 
 
-def common_elements(array_1, array_2):
+def common_elements(array_1: list, array_2: list) -> list:
     # Note: this solution don't use the fact that array_1 and array_2 are sorted.
     array_1_elts_in_array_2 = [elt for elt in array_1 if elt in array_2]
     common_elts_set = set(array_1_elts_in_array_2)
@@ -14,4 +14,6 @@ def common_elements(array_1, array_2):
 
 
 if __name__ == '__main__':
-    pass
+    array_1 = [1, 2, 4, 6, 7]
+    array_2 = [2, 3, 6, 8]
+    print(common_elements(array_1, array_2))
