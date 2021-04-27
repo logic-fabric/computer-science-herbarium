6b3c018ddc05a17f36635a699277ba3426c7116f
logic-fabric <lowic@logic-fabric.fr>
2021-04-27 06:53:21 +0200

    test: add unit tests to commonElements() with Jasmine JS

--- classic-puzzles/puzzles-with-arrays/commonElements.js
+++ classic-puzzles/puzzles-with-arrays/commonElements.js
@@ -1,63 +1,36 @@
+"use strict";
+
 /*
 Wording:
 -------
 Find the common elements (as a sorted array) between two sorted arrays of integers.
 */
 
+/**
+ * Returns common elements between two sorted arrays, as a sorted array with no duplicate.
+ * @param {Array.number} array1
+ * @param {Array.number} array2
+ * @returns
+ */
 function commonElements(array1, array2) {
-    let commonElts = [];
-    let pointer1 = 0;
-    let pointer2 = 0;
+  let commonElts = [];
+  let pointer1 = 0;
+  let pointer2 = 0;
 
-    while (pointer1 < array1.length && pointer2 < array2.length) {
-        if (commonElts.includes(array1[pointer1])) {
-            pointer1++;
-        } else if (array1[pointer1] == array2[pointer2]) {
-            commonElts.push(array1[pointer1]);
-            pointer1++;
-            pointer2++;
-        } else if (array1[pointer1] < array2[pointer2]) {
-            pointer1++;
-        } else {
-            pointer2++;
-        }
+  while (pointer1 < array1.length && pointer2 < array2.length) {
+    if (commonElts.includes(array1[pointer1])) {
+      pointer1++;
+    } else if (array1[pointer1] == array2[pointer2]) {
+      commonElts.push(array1[pointer1]);
+      pointer1++;
+      pointer2++;
+    } else if (array1[pointer1] < array2[pointer2]) {
+      pointer1++;
+    } else {
+      pointer2++;
     }
-    return commonElts
+  }
+  return commonElts;
 }
 
-// Tests suite:
-console.log("Test with an empty array:");
-console.log(
-    "Result:", commonElements(
-        [1, 2, 4, 6, 7],
-        []
-    ),
-    "Expected: []"
-);
-
-console.log("Test arrays with common elements");
-console.log(
-    "Result:", commonElements(
-        [1, 2, 4, 6, 7],
-        [2, 3, 6, 8]
-    ),
-    "Expected: [2, 6]"
-);
-
-console.log("Test arrays with no common elements");
-console.log(
-    "Result:", commonElements(
-        [1, 2, 4, 6, 7],
-        [3, 5, 8]
-    ),
-    "Expected: []"
-);
-
-console.log("Test arrays with duplicated elements");
-console.log(
-    "Result:", commonElements(
-        [1, 2, 2, 4, 6, 6, 6, 7],
-        [2, 2, 2, 3, 6, 6, 8]
-    ),
-    "Expected: [2, 6]"
-);
+module.exports = commonElements;
