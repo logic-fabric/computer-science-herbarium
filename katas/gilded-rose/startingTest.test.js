371d5b5f91ee6bc78d0b79f9b43ea0deb7a77327
logic-fabric <lowic@logic-fabric.fr>
2022-11-27 10:59:12 +0100

    feature: add Gilded Rose initial code, test and fixture

---
+++ katas/gilded-rose/startingTest.test.js
@@ -0,0 +1,9 @@
+const { Shop, Item } = require("./startingCode.js");
+
+describe("Gilded Rose", function () {
+  it("should foo", function () {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("fixme");
+  });
+});
