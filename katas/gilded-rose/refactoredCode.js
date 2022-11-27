1fcf7f742dbe02cf78d058506a67b0b714479b25
logic-fabric <lowic@logic-fabric.fr>
2022-11-27 22:32:28 +0100

    refactor: simplify expressions to update item quality

--- katas/gilded-rose/refactoredCode.js
+++ katas/gilded-rose/refactoredCode.js
@@ -18,39 +18,39 @@ class Shop {
         item.name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
         if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
-          item.quality = item.quality - 1;
+          item.quality--;
         }
       } else {
         if (item.quality < 50) {
-          item.quality = item.quality + 1;
+          item.quality++;
 
           if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
             if (item.sellIn < 11 && item.quality < 50) {
-              item.quality = item.quality + 1;
+              item.quality++;
             }
             if (item.sellIn < 6 && item.quality < 50) {
-              item.quality = item.quality + 1;
+              item.quality++;
             }
           }
         }
       }
 
       if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn = item.sellIn - 1;
+        item.sellIn--;
       }
 
       if (item.sellIn < 0) {
         if (item.name != "Aged Brie") {
           if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
             if (item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros") {
-              item.quality = item.quality - 1;
+              item.quality--;
             }
           } else {
-            item.quality = item.quality - item.quality;
+            item.quality = 0;
           }
         } else {
           if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality++;
           }
         }
       }
