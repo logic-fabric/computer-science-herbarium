58f9b1440d18521cc2ef5a50557ffa62ec57a0a7
logic-fabric <lowic@logic-fabric.fr>
2022-11-27 11:16:58 +0100

    feature: add files to refactor Gilded Rose

--- katas/gilded-rose/goldenMaster.test.js
+++ katas/gilded-rose/goldenMaster.test.js
@@ -1,9 +1,10 @@
 const { Shop, Item } = require("./startingCode.js");
 
 describe("Gilded Rose", function () {
-  it.skip("should foo", function () {
+  it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+
+    expect(items[0].name).toBe("foo");
   });
 });
---
+++ katas/gilded-rose/updateQualityExplorer.js
@@ -0,0 +1,38 @@
+const { Shop, Item } = require("./startingCode.js");
+
+const items = [
+  new Item("+5 Dexterity Vest", 10, 20),
+  new Item("Aged Brie", 2, 0),
+  new Item("Elixir of the Mongoose", 5, 7),
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+
+  // This Conjured item does not work properly yet
+  new Item("Conjured Mana Cake", 3, 6),
+];
+
+const days = Number(process.argv[2]) || 2;
+const gildedRose = new Shop(items);
+
+for (let day = 0; day < days; day++) {
+  console.log(`\n-------- day ${day} --------`);
+  console.log(
+    `${"name".padEnd(42, " ")} | ${"sellIn".padEnd(
+      8,
+      " "
+    )} | ${"quality".padEnd(8, " ")}`
+  );
+
+  items.forEach((item) =>
+    console.log(
+      `${item.name.padEnd(42, " ")} | ${item.sellIn
+        .toString()
+        .padStart(8, " ")} | ${item.quality.toString().padStart(8, " ")}`
+    )
+  );
+
+  gildedRose.updateQuality();
+}
