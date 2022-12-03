4ff7d8a67731e13bdafb814c97ee79e3807ab492
logic-fabric <lowic@logic-fabric.fr>
2022-12-03 06:37:45 +0100

    refactor: Backstage pass case treated apart

--- katas/gilded-rose/refactoredCode.js
+++ katas/gilded-rose/refactoredCode.js
@@ -14,7 +14,14 @@ class Shop {
   updateItem(item) {
     if (item.name === "Aged Brie") {
       item.sellIn--;
-      item.quality += item.sellIn < 0 ? 2 : 1;
+
+      if (item.sellIn < 0) {
+        item.quality += 2;
+      } else {
+        item.quality += 1;
+      }
+
+      item.quality = Math.min(50, item.quality);
 
       return item;
     }
@@ -23,32 +30,34 @@ class Shop {
       return item;
     }
 
-    if (
-      item.name !== "Backstage passes to a TAFKAL80ETC concert" &&
-      item.quality > 0
-    ) {
-      item.quality--;
-    } else {
-      if (item.quality < 50) {
-        item.quality++;
+    if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+      item.sellIn--;
 
-        if (
-          item.name === "Backstage passes to a TAFKAL80ETC concert" &&
-          item.sellIn < 11 &&
-          item.quality < 50
-        ) {
-          item.quality++;
-        }
+      if (item.sellIn < 0) {
+        item.quality = 0;
+      } else if (item.sellIn < 5) {
+        item.quality += 3;
+      } else if (item.sellIn < 10) {
+        item.quality += 2;
+      } else {
+        item.quality += 1;
       }
+
+      item.quality = Math.min(50, item.quality);
+
+      return item;
+    }
+
+    if (item.quality > 0) {
+      item.quality--;
+    } else if (item.quality < 50) {
+      item.quality++;
     }
 
     item.sellIn--;
 
     if (item.sellIn < 0) {
-      if (
-        item.name !== "Backstage passes to a TAFKAL80ETC concert" &&
-        item.quality > 0
-      ) {
+      if (item.quality > 0) {
         item.quality--;
       } else {
         item.quality = 0;
